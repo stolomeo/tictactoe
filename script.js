972cb9d4740f53dce2b43c72776ef08d51300919
stolomeo <tolomeosebastian@gmail.com>
2022-03-20 16:31:31 -0400

    Add gameController module and player factory function

--- script.js
+++ script.js
@@ -13,52 +13,6 @@ const gameBoard = (() => {
     gameBoard[gameBoardID] = currentTurn;
   };
 
-  const checkResult = () => {
-    if (gameboard[0] == "X" && gameboard[1] == "X" && gameboard[2] == "X") {
-    }
-
-    if (gameboard[0] == "O" && gameboard[1] == "O" && gameboard[2] == "O") {
-    }
-
-    if (gameboard[3] == "X" && gameboard[4] == "X" && gameboard[5] == "X") {
-    }
-
-    if (gameboard[3] == "O" && gameboard[4] == "O" && gameboard[5] == "O") {
-    }
-
-    if (gameboard[6] == "X" && gameboard[7] == "X" && gameboard[8] == "X") {
-    }
-
-    if (gameboard[6] == "O" && gameboard[7] == "O" && gameboard[8] == "O") {
-    }
-
-    if (gameboard[0] == "X" && gameboard[3] == "X" && gameboard[6] == "X") {
-    }
-
-    if (gameboard[0] == "O" && gameboard[3] == "O" && gameboard[6] == "O") {
-    }
-
-    if (gameboard[1] == "X" && gameboard[4] == "X" && gameboard[7] == "X") {
-    }
-
-    if (gameboard[1] == "O" && gameboard[4] == "O" && gameboard[7] == "O") {
-    }
-
-    if (gameboard[2] == "X" && gameboard[5] == "X" && gameboard[8] == "X") {
-    }
-
-    if (gameboard[2] == "O" && gameboard[5] == "O" && gameboard[8] == "O") {
-    }
-    if (gameboard[0] == "X" && gameboard[4] == "X" && gameboard[8] == "X") {
-    }
-    if (gameboard[0] == "O" && gameboard[4] == "O" && gameboard[8] == "O") {
-    }
-    if (gameboard[2] == "X" && gameboard[4] == "X" && gameboard[6] == "X") {
-    }
-    if (gameboard[2] == "O" && gameboard[4] == "O" && gameboard[6] == "O") {
-    }
-  };
-
   return {
     getGameBoard,
     updateGameBoard,
@@ -105,8 +59,6 @@ const gameController = (() => {
   const placeMark = (square, currentTurn) => {
     square.textContent = currentTurn;
   };
-
-  const endGame = () => {};
 
   return {
     determineTurn,
